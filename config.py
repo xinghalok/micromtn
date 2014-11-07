f7352ec00585b8ea5b97cf18bd2d3517b7eaaad1
Michael Anzuoni <manzuoni@appnexus.com>
2014-11-06 22:27:52 -0500

    Cleanup and new beginnings

--- config.py
+++ config.py
@@ -2,18 +2,18 @@
 
 # ADD A FUNCTION THAT RANDOMLY SELECTS A STRING FROM A LIST
 # AND THEN MAKES IT THE WEBSITE NAME
-WEBSITE_NAME = 'mea'
+WEBSITE_NAME = "Michael Anzuoni's cool home on the web!"
 DEBUG = True
 
 # ARTICLE AND PAGE CONFIGURATION
 
-ARTICLE_DIR = '/var/www/micromtn/articles/'
-PAGES_DIR = '/var/www/micromtn/pages/'
+ARTICLE_DIR = './articles/'
+PAGES_DIR = './pages/'
 
 # NAVBAR CONFIGURATION
 # Add navbar links with dict(title="Title", url="URL)
 
-NAV = [dict(title='Home', url='/'), dict(title="Resume", url="/resume"), dict(title="Code", url="https://github.com/yolesaber")]
+NAV = [dict(title="C V", url="/resume"), dict(title="c0des", url="https://github.com/yolesaber")]
 
 # SECURITY CONFIGURATION
 
